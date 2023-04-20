3fda56a0a2617fd56831b6ad72bb492607743d45
jstreeter818 <jadestreeter@unomaha.edu>
2023-04-20 10:45:18 -0500

    class methods

--- account.py
+++ account.py
@@ -1,1 +1,26 @@
-# account class
+class Account:
+    def __init__(self, name):
+        self.__account_name = name
+        self.__account_balance = 0
+
+    def deposit(self, amount):
+        self.get_balance()
+        if amount > 0:
+            self.__account_balance += amount
+            return True
+        else:
+            return False
+
+    def withdraw(self, amount):
+        self.get_balance()
+        if 0 < amount <= self.__account_balance:
+            self.__account_balance -= amount
+            return True
+        else:
+            return False
+
+    def get_balance(self):
+        return self.__account_balance
+
+    def get_name(self):
+        return self.__account_name
